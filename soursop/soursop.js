175e12a66d007e40970fc797c2cbb516f672c2e0
Chris Oh <oh2@hawaii.edu>
2017-11-18 13:12:28 -1000

    added npmstart and open index.html

--- soursop/soursop.js
+++ soursop/soursop.js
@@ -1,8 +1,14 @@
-// var http = require('http');
-//
-// var server = http.createServer(function(req, res) {
-//   res.writeHead(200);
-//   res.end('Hello Http');
-// });
-// server.listen(8080);
+var http = require('http'),
+     fs = require('fs');
 
+
+ fs.readFile('./index.html', function (err, html) {
+     if (err) {
+         throw err;
+     }
+     http.createServer(function(request, response) {
+         response.writeHeader(200, {"Content-Type": "text/html"});
+         response.write(html);
+         response.end();
+     }).listen(8080);
+ });
