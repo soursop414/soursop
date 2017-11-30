785c9ba676b0eec2e3b65afb8d40f17dbac75217
chadmorita <chadmmm@hawaii.edu>
2017-11-29 22:04:53 -1000

    Attempting to merge vis and meteor stuff

--- meteor/app/imports/ui/pages/index.js
+++ meteor/app/imports/ui/pages/index.js
@@ -2,3 +2,5 @@ import './home-page.html';
 import './home-page.js';
 import './create-event-modal.html';
 import './create-event-modal.js';
+import './vis.html';
+import './vis.js';
---
+++ meteor/app/imports/ui/pages/vis.js
@@ -0,0 +1,41 @@
+import { Template } from 'meteor/templating';
+import { DataSet, Timeline } from 'vis';
+
+
+Template.Vis.onRendered(function onRendered() {
+
+  // DOM element where the Timeline will be attached
+  const container = this.document.getElementById('visualization');
+
+  // Create a DataSet (allows two way data-binding)
+  const items = new DataSet([
+    { id: 1, content: 'item 1', start: '2014-04-20', end: '2014-04-22', className: 'cp' },
+    { id: 2, content: 'item 2', start: '2014-04-14', end: '2014-04-16', className: 'cp' },
+    { id: 3, content: 'item 3', start: '2014-04-18', end: '2014-04-19' },
+    { id: 4, content: 'item 4', start: '2014-04-16', end: '2014-04-18' },
+    { id: 5, content: 'item 5', start: '2014-04-25', end: '2014-04-26' },
+  ]);
+
+  // const newItems = new DataSet([
+  //   { id: 1, content: 'new', start: '2014-04-20', end: '2014-04-25' },
+  //   { id: 2, content: 'new 2', start: '2014-04-14', end: '2014-04-16' },
+  //   { id: 3, content: 'new 3', start: '2014-04-18', end: '2014-04-19' },
+  //   { id: 4, content: 'new 4', start: '2014-04-16', end: '2014-04-18' },
+  //   { id: 5, content: 'new 5', start: '2014-04-25', end: '2014-04-26' },
+  // ]);
+
+  // Configuration for the Timeline
+  const options = {};
+
+  console.log(container);
+  // Create a Timeline
+  const timeline = new Timeline(container, items, options);
+
+  //onclick
+  timeline.on('click', function (properties)
+  {
+  console.log(properties.item);
+  //timeline.setItems(newItems); //can edit the dataset
+  });
+
+});
